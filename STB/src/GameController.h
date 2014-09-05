9e462584efede53b0a5b0f547f70b1809a5e6e3b
danielvandenberg95 <danielvandenberg95@gmail.com>
2014-09-05 13:04:52 +0200

    Added comments

--- STB/src/GameController.h
+++ STB/src/GameController.h
@@ -9,7 +9,15 @@ public:
 		return instance;
 	}
 
+	//start (run) the game
+	//
+	//Call this function to start running the game.
+	//@returns the exit code.
 	int GameController::start();
+
+	//stop the game
+	//
+	//This function will stop the game. Prerequirement: start() must be called.
 	void GameController::stop();
 private:
 	//Singleton creation
