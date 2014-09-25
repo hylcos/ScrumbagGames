18f1a80420fee7998adfb4b3e4ada0d999bd9c38
danielvandenberg95 <danielvandenberg95@gmail.com>
2014-09-25 12:27:15 +0200

    Fixed TextureManager

--- STB/src/TextureManager.cpp
+++ STB/src/TextureManager.cpp
@@ -1,11 +1,24 @@
 #include "stdafx.h"
 #include "TextureManager.h"
+#include <map>
 
 
 TextureManager::TextureManager()
 {
 }
 
+static sf::Texture getTexture(std::string name){
+	std::map<std::string, sf::Texture>::iterator it =TextureManager::map.find(name);
+	if (it != TextureManager::map.end())
+	{
+		//element found;
+		return it->second;
+	}
+	sf::Texture tex;
+	tex.loadFromFile("Resources/Images/" + name);
+	TextureManager::map.insert(std::pair<std::string,sf::Texture>(name, tex));
+	return tex;
+}
 
 TextureManager::~TextureManager()
 {
