40cf91fae359ad6f046b41176519130c612babc4
danielvandenberg95 <danielvandenberg95@gmail.com>
2014-09-25 10:50:21 +0200

    Added methods to GameObject

--- STB/src/gameObjects/GameObject.cpp
+++ STB/src/gameObjects/GameObject.cpp
@@ -1,8 +1,14 @@
 #include "../stdafx.h"
 #include "GameObject.h"
+#include <SFML/Graphics.hpp>
 
 
 GameObject::GameObject()
+{
+}
+
+GameObject::GameObject(sf::Vector2f pos) :
+position{pos}
 {
 }
 
@@ -13,6 +19,10 @@ void GameObject::update(float speedModifier){
 void GameObject::setPosition(sf::Vector2f position){
 	GameObject::position.x = position.x;
 	GameObject::position.y = position.y;
+}
+
+sf::Vector2f GameObject::getPosition(){
+	return position;
 }
 
 void GameObject::move(float speedModifier){
--- STB/src/gameObjects/GameObject.h
+++ STB/src/gameObjects/GameObject.h
@@ -5,14 +5,17 @@ class GameObject
 {
 public:
 	GameObject();
+	GameObject(sf::Vector2f);
 
 	virtual void GameObject::update(float speedModifier);
 	virtual void GameObject::move(float speedModifier);
 	virtual void GameObject::draw(sf::RenderWindow & window) const;
 	void GameObject::setPosition(sf::Vector2f position);
+	sf::Vector2f GameObject::getPosition();
 
 	virtual ~GameObject();
 protected:
 	sf::Vector2f position;
+	bool needsRedraw;
 };
 
