64d838c6f4496815a5cc06ac866415c91ad13f65
danielvandenberg95 <danielvandenberg95@gmail.com>
2014-09-25 12:16:17 +0200

    Added TextureManager

--- STB/src/TextureManager.h
+++ STB/src/TextureManager.h
@@ -1,12 +1,14 @@
 #pragma once
 #include <string>
 #include <SFML/Graphics.hpp>
+#include <map>
 
 class TextureManager
 {
 public:
 	TextureManager();
 	static sf::Texture getTexture(std::string name);
+	static 
 	~TextureManager();
 };
 
