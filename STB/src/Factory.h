9e4009a066dcfa9e7b53f9a097040062784a0eb7
danielvandenberg95 <danielvandenberg95@gmail.com>
2014-09-26 14:42:21 +0200

    Documented the Factory level settings

--- STB/src/Factory.h
+++ STB/src/Factory.h
@@ -16,6 +16,23 @@ class Factory
 {
 public:
 	enum levelSettings{
+		/*	Usage:
+			bits:	0000000011111111: terror level (0-255)
+			bit:	0000000100000000: random (256)
+			bit:	0000001000000000: unused (512)
+			bit:	0000010000000000: unused (1024)
+			bit:	0000100000000000: unused (2048)
+			bit:	0001000000000000: unused (4096)
+			bit:	0010000000000000: unused (8192)
+			bit:	0100000000000000: unused (16384)
+			bit:	1000000000000000: unused (32768)
+			Calculating the level setting value:
+				add all values and settings together, and write this to the first line of the .level file.
+			Calculating if an option is set:
+				(settings & option) == option
+			Calculating if multiple options are set:
+				(settings & (option1 | option2 | option3)) == option1 | option2 | option3
+		*/
 		//256
 		//2-512
 		//3-1024
@@ -24,7 +41,7 @@ public:
 		//6-8192
 		//7-16384
 		//8-32768
-		random = 512
+		random = 256
 	};
 	Factory();
 	int Factory::loadLevel(std::string file, LevelController & levelController);
@@ -33,4 +50,3 @@ public:
 private:
 	GameObjectManager gameObjectManager;
 };
-
