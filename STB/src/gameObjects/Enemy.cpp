9073829801c190f6ddcdb810dc2de7e0b5aa4f99
danielvandenberg95 <danielvandenberg95@gmail.com>
2014-09-29 12:15:10 +0200

    Removed one variable

--- STB/src/gameObjects/Enemy.cpp
+++ STB/src/gameObjects/Enemy.cpp
@@ -23,8 +23,7 @@ Enemy::Enemy(Enemy::Initializer initializer)
 }
 
 void Enemy::update(float speedModifier){
-	LevelController& levelController = LevelController::getInstance();
-	Player* player = levelController.getPlayer();
+	Player* player = LevelController::getInstance().getPlayer();
 
 	sprite.setRotation(atan2(position.y - player->getPosition().y, position.x - player->getPosition().x) * 180 / 3.14159265358979323846f - 90);
 }
