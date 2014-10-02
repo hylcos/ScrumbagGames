636710506ca96b7cd5c6d0f00cfcfd3cbf48de41
areitsma <arnout.reitsma@student.hu.nl>
2014-10-02 13:59:22 +0200

    Enemy intialize

--- STB/src/gameObjects/Enemy.h
+++ STB/src/gameObjects/Enemy.h
@@ -31,8 +31,15 @@ public:
 	void Enemy::update(float speedModifier) override;
 	void Enemy::draw(sf::RenderWindow & window) const override;
 	void Enemy::move(float speedModifier) override;
-	Initializer average{ "Average", 0.9f, 100, true, 20, 60 };
 
+	Initializer average{ "Average", 0.9f, 100, true, 20, 60 };
+	Initializer fat{ "Fat", 0.7f, 300, true, 20, 60 };
+	Initializer cheerleader{ "Cheerleader", 1.3f, 50, true, 20, 100 };
+	Initializer horde{ "Horde", 0.7f, 70, true, 20, 70 };
+	Initializer macho{ "Macho", 0.8f, 100, true, 40, 30 };
+
+	Initializer dunken{ "Boss Dunken", 0.9f, 1500, true, 20, 60 };
+	
 	~Enemy();
 private:
 	float hitCooldown = 0;
