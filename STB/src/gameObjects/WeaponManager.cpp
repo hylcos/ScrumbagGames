23181b1558620b328b5e856b91bbbe2db79e2d8b
danielvandenberg95 <danielvandenberg95@gmail.com>
2014-10-29 15:24:38 +0100

    Increased bullet speed

--- STB/src/gameObjects/WeaponManager.cpp
+++ STB/src/gameObjects/WeaponManager.cpp
@@ -5,10 +5,10 @@ void WeaponManager::load()
 		return;
 	}
 	isLoaded = true;
-	pistol = new Gun("pistol", 25, 90.0, 8, 8, 350, 2, 30);
-	rifle = new Gun("rifle", 34, 90.0, 120, 30, 350, 2, 15);
-	shotgun = new Gun("shotgun", 50, 180.0, 60, 6, 350, 10, 45);
-	sniper = new Gun("sniper", 100, 180.0, 50, 4, 999, 15, 60);
+	pistol = new Gun("pistol", 25, 90.0, 8, 8, 350, 8, 30);
+	rifle = new Gun("rifle", 34, 90.0, 120, 30, 350, 8, 15);
+	shotgun = new Gun("shotgun", 50, 180.0, 60, 6, 350, 20, 45);
+	sniper = new Gun("sniper", 100, 180.0, 50, 4, 999, 30, 60);
 	dagger = new Knife("dagger", 34, 70, 16);
 	sword = new Knife("sword", 50, 70, 32);
 
