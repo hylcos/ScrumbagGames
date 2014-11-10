0232b3a02e8ec8f96deb61455d9f8f1da9a6bfa4
Timhasselaar <tim.hasselaar@student.hu.nl>
2014-11-10 01:16:04 +0100

    pffff

--- STB/src/SoundController.h
+++ STB/src/SoundController.h
@@ -1,3 +1,8 @@
+//! The sound controller header file
+/*!
+The header file of the sound controller
+*/
+
 #pragma once
 #include <SFML/Audio.hpp>
 #include <iostream>
@@ -5,6 +10,10 @@
 class SoundController
 {
 public:
+	// The get instance method of the sound controller
+	/*!
+	@return a reference to the instance of the sound controller
+	*/
 	static SoundController& getInstance()
 	{
 		static SoundController    instance;// Guaranteed to be destroyed.
@@ -13,25 +22,53 @@ public:
 		return instance;
 	}
 
+	//! The file placement of the intro music
 	const char* INTRO = "Resources/Sounds/intro.ogg";
+
+	//! The file placement of the outro music
 	const char* OUTRO = "Resources/Sounds/outro.ogg";
+
+	//! The load method of the sound controller
+	/*!
+	Loads the song from the given file location and sets its volume to the volume saved in the settings.
+	*/
 	void SoundController::load();
-	//play selected music
-	//
-	//Call this to start playing the selected music.
+
+	//! THe play music method of the sound controller
+	/*!
+	This method will play the selected music
+	@param file The music that should be played.
+	*/
 	void SoundController::playMusic(const std::string file );
 
-	//step for the controller
-	//
-	//Executed every step to check change requests in the music
-	void SoundController::step();
-
+	//! the set background music method of the sound controller
+	/*!
+	This method will set the background music on or off
+	@param enabled Wether or not the background music should be enabled
+	*/
 	void SoundController::setBackgroundMusic(bool enabled);
+
+	//! the set background music volume method of the sound controller
+	/*!
+	This method will set the volume of the background music that is playing
+	@param value The level of sound of the background music
+	*/
 	void SoundController::setBackgroundMusicVolume(int value);
+
+	//! The set sound volume method of the sound controller
+	/*!
+	This method will set the volume level of all the other sounds in the game
+	@param value The level that the volume of the sounds should be set to.
+	*/
 	void SoundController::setSoundVolume(int value);
 
+	//! The deconstructor of the sound controller class
+	/*!
+	A default deconstructor
+	*/
 	~SoundController();
 
+	//! A map saving the name of a song, and a pointer to the sound file
 	std::map <  std::string, sf::Music* > map;
 private:
 	SoundController() {};
